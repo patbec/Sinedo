66723f9e277ed592522cf73fc8c8f3dbd5c5483e
Patrick <29308797+patbec@users.noreply.github.com>
2022-02-12 12:34:12 +0100

    Aufgeräumt.

--- src/Sinedo/Pattern/Middleware/WebSocketRouting.cs
+++ src/Sinedo/Pattern/Middleware/WebSocketRouting.cs
@@ -97,64 +97,30 @@ namespace Sinedo.Middleware
             if (!httpContext.User.Identity.IsAuthenticated)
             {
                 _logger.LogWarning("Client with IP {ipAddress} has sent a request without a valid login. (401 - Unauthorized)", httpContext.Connection.RemoteIpAddress);
-
-                // Fehlercode zurückgeben: 401 - Nicht angemeldet.
                 return HttpStatusCode.Unauthorized;
             }
-
-            // if (!httpContext.Request.Query.ContainsKey("version"))
-            // {
-            //     _logger.LogWarning("Client with IP {ipAddress} has sent a request without the required version parameter. (400 - BadRequest)", httpContext.Connection.RemoteIpAddress);
-
-            //     // Fehlercode zurückgeben: 400 - Fehlender Parameter in der Url.
-            //     return HttpStatusCode.BadRequest;
-            // }
-
-            // if (httpContext.Request.Query["version"] != AVAILABLE_API_VERSION)
-            // {
-            //     _logger.LogWarning("Client with IP {ipAddress} has requested Api version {requestedApiVersion}. This server supports only Api version {supportedApiVersion}. (501 - NotImplemented)", httpContext.Connection.RemoteIpAddress, httpContext.Request.Query["version"], AVAILABLE_API_VERSION);
-
-            //     // Fehlercode zurückgeben: 501 - Die angefragte Api-Version wird von diesem Server nicht unterstützt.
-            //     return HttpStatusCode.NotImplemented;
-            // }
 
             if (!httpContext.Request.Query.ContainsKey("channels"))
             {
                 _logger.LogWarning("Client with IP {ipAddress} has sent a request without the required parameter channels. (400 - BadRequest)", httpContext.Connection.RemoteIpAddress);
-
-                // Fehlercode zurückgeben: 400 - Fehlender Parameter in der Url.
                 return HttpStatusCode.BadRequest;
             }
 
             if (!httpContext.Request.Query.ContainsKey("friendlyName"))
             {
                 _logger.LogWarning("Client with IP {ipAddress} has sent a request without the required parameter friendlyName. (400 - BadRequest)", httpContext.Connection.RemoteIpAddress);
-
-                // Fehlercode zurückgeben: 400 - Fehlender Parameter in der Url.
                 return HttpStatusCode.BadRequest;
             }
 
             if (string.IsNullOrWhiteSpace(httpContext.Request.Query["friendlyName"]))
             {
                 _logger.LogWarning("Client with IP {ipAddress} has sent a request with empty query parameter friendlyName. (400 - BadRequest)", httpContext.Connection.RemoteIpAddress);
-
-                // Fehlercode zurückgeben: 400 - Ungültiger Wert in einem Url-Parameter.
                 return HttpStatusCode.BadRequest;
             }
-
-            // if (!(httpContext.Request.Headers.ContainsKey("Accept") && httpContext.Request.Headers["Accept"] == "application/octet-stream"))
-            // {
-            //     _logger.LogWarning("Client with IP {0} has sent a request without the required Accept header value application/octet-stream. (406 - NotAcceptable)", httpContext.Connection.RemoteIpAddress);
-
-            //     // Fehlercode zurückgeben: 406 - Nicht unterstützter Accept Header.
-            //     return HttpStatusCode.NotAcceptable;
-            // }
 
             if (!httpContext.WebSockets.IsWebSocketRequest)
             {
                 _logger.LogWarning("Client with IP {ipAddress} must request with a WebSocket connection. (505 - HttpVersionNotSupported)", httpContext.Connection.RemoteIpAddress);
-
-                // Fehlercode zurückgeben: 505 - Keine WebSocket (HTTP2) Verbindung.
                 return HttpStatusCode.HttpVersionNotSupported;
             }
 
@@ -163,8 +129,6 @@ namespace Sinedo.Middleware
             if (webSocketChannels == null)
             {
                 _logger.LogWarning("Client with IP {ipAddress} has requested a channel that is not supported. The following channels are available: {supportedChannels} (501 - NotImplemented)", httpContext.Connection.RemoteIpAddress, string.Join(", ", Enum.GetNames(typeof(WebSocketChannel))));
-
-                // Fehlercode zurückgeben: 501 - Nicht unterstützte(r) Channel in der Url.
                 return HttpStatusCode.NotImplemented;
             }
 
@@ -186,9 +150,9 @@ namespace Sinedo.Middleware
 
             for (int i = 0; i < stringChannels.Length; i++)
             {
-                bool isSuccessfully = Enum.TryParse(stringChannels[i], out WebSocketChannel channel);
+                bool isSuccessful = Enum.TryParse(stringChannels[i], out WebSocketChannel channel);
 
-                if (!isSuccessfully)
+                if (!isSuccessful)
                 {
                     return null;
                 }
